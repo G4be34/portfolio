e2ce756f1599a5239459bd9bf848d0f2b2e8d0dd
Gabe Jimenez <jimenezgabriel12@gmail.com>
2025-02-12 14:04:54 -0700

    Update footer label

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 
 export default function Footer() {
   return (
     <footer className='mb-10 px-4 text-center text-gray-500'>
       <small className='mb-2 block text-xs'>
-        &copy; 2030 Gab. All rights reserved.
+        &copy; 2025 Gabriel Jimenez. All rights reserved.
       </small>
       <p className='text-xs'>
         <span className='font-semibold'>About this website:</span> built with React & Next.js (App Router & Server Actions), Typescript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel for hosting.
